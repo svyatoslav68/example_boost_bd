24b4c6d06a97326867b7c1c76f1beca60f45c712
Svyatoslav <svyatoslav68@icloud.com>
2022-05-10 08:10:30 +0300

    Можно выполнить запрос, возвращающий одну запись и она выводиться на экран

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <boost/tuple/tuple.hpp>
 #include "cli_options.hpp"
 #include "connection.hpp"
 
@@ -9,9 +10,13 @@ int main(int argc, char **argv)
 	CliOptions options(argc, argv);
 	std::cout << "User: " << options.getUser() << "; Password: " << options.getPassword() << std::endl;
 	std::cout << "Database = " << options.getDatabase() << std::endl;
+	socibd::Connection con;
+	socibd::Connection::ParametrsBD parBD{options.getDatabase(), options.getUser(), options.getPassword()};
+	con.open(socibd::Connection::POSTGRES_SERVER, parBD);
 	std::string SQL;
 	std::getline(std::cin, SQL);
 	std::cout << "SQL: " << SQL << std::endl;
-	socibd::Connection con;
+	boost::tuple<int, std::string> record = con.getRecord<int, std::string>(SQL);
+	std::cout << "0:" << record.get<0>() << "; 1:" << record.get<1>() << std::endl;
 }
 
